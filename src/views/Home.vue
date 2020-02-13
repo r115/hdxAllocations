fb3e03f18d52ebbd4af699e658274c33f2b7a5d2
Bernard Nandwa <bernard.nandwa@andela.com>
2020-02-13 17:01:31 +0300

    Add tailwind to the project.
    Missed some files in previous commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container max-w-full h-screen flex flex-row">
+    <div class="w-1/5 bg-white h-full">
+      Sidebar
+    </div>
+    <div class="w-4/5">
+      Main Body
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+
   },
 };
 </script>
